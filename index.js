e5be7222b14a8ff648382697e9f6cc7e1e7c2aa8
Matt Glick <matt.glick@dowjones.com>
2015-06-22 14:22:10 -0400

    removed the asinSearcher, formatted some things, removed some commented out code. Updated some names to make things cleaner.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * Created by glickm on 6/19/15.
  */
-module.exports = run = require('./lib/services/evaluator');
-_ = require('underscore');
+exports.evaluator = require('./lib/services/evaluator');
+exports.keepaFacade = require('./lib/facades/keepa-facade');
 
-run.evaluator("B00IUD7I5O", function (results) {
+exports.evaluator.evaluate("B00J3LXMOS", function (results) {
   console.log(results);
 });
 
