dc5c3e0c5adeba9e2cfd055fca799b434fd5c020
Matt Glick <matt.glick@dowjones.com>
2015-06-22 13:52:05 -0400

    moved the asin searcher into the evaluator entirely. added the functionality to evaluate the products ROI and rank.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = run = require('./lib/services/evaluator');
 _ = require('underscore');
 
-run.evaluator("B00FCW88WI", function (results) {
+run.evaluator("B00IUD7I5O", function (results) {
   console.log(results);
 });
 
