b11d7268f4d11e77a93a13a316418b0c2e9028fa
Matt Glick <matt.glick@dowjones.com>
2015-06-23 15:49:11 -0400

    Added camelcamelcamel scraper to retrieve the top percentage deals for today. currently overloading amazon seller requests though. needs to be reworked.

--- index.js
+++ index.js
@@ -3,10 +3,21 @@
  */
 exports.evaluator = require('./lib/services/evaluator');
 exports.keepaFacade = require('./lib/facades/keepa-facade');
+exports.cccFacade = require('./lib/facades/ccc-facade');
 exports.twitterMonitor = require('./lib/services/twitter-monitor');
-
-//exports.evaluator.evaluate("B00KDZGBGE", function (results) {
+//
+//exports.evaluator.evaluate("B00N22CQ32", function (results) {
 //  console.log(results);
 //});
 
-exports.twitterMonitor.monitorKeepa();
+//exports.twitterMonitor.monitorKeepa();
+
+exports.cccFacade.scrape(function (asinArray) {
+  asinArray.forEach(function (asin) {
+    exports.evaluator.evaluate(asin, function (result) {
+      if(result.determination.buy === 'true') {
+        console.log(result);
+      }
+    })
+  })
+});
