67a85560caa3fdf5d953840322377b387ba2af25
Matt Glick <matt.glick@dowjones.com>
2015-06-23 00:07:56 -0400

    added the twitter monitor starting code. should monitor twitter, pull down the keepa url, grab the amazon asin, and send it to the evaluator. still would like a way for the user stream to ONLY pull down keepa charts

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
  */
 exports.evaluator = require('./lib/services/evaluator');
 exports.keepaFacade = require('./lib/facades/keepa-facade');
+exports.twitterMonitor = require('./lib/services/twitter-monitor');
 
-exports.evaluator.evaluate("B00J3LXMOS", function (results) {
-  console.log(results);
-});
+//exports.evaluator.evaluate("B00KDZGBGE", function (results) {
+//  console.log(results);
+//});
 
+exports.twitterMonitor.monitorKeepa();
