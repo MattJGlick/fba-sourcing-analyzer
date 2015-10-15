97978d1e3a1c16549427bec7579335c6119a7b31
Matt Glick <matt.glick@dowjones.com>
2015-10-15 13:53:34 -0400

    moved the scheduler to its own file

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
  */
 
 var http = require('http');
-var schedule = require('node-schedule');
 var express = require('express');
 
 var Queue = require('./lib/queue');
 
 exports.evaluator = evaluator = require('./lib/services/evaluator');
-exports.cccFacade = cccFacade = require('./lib/facades/ccc-facade');
+exports.scheduler = scheduler = require('./lib/services/scheduler');
 exports.twitterMonitor = twitterMonitor = require('./lib/services/twitter-monitor');
 exports.mailer = mailer = require('./lib/services/mailer');
 exports.mwsFacade =  require('./lib/facades/mws-facade');
@@ -27,31 +26,10 @@ server.listen(process.env.PORT || 5000);
 
 logger.log("Running on Port: " + (process.env.PORT || "5000"));
 
+var queue = new Queue();
 
-var q = new Queue();
-
-exports.twitterMonitor.monitorKeepa(q);
-
-// run the CCC stuff every 4 hours. I think I want to move this out of here?
-schedule.scheduleJob('0 */4 * * *', function(){
-  console.log(JSON.stringify("Starting Cron Section", null, 2));
-  cccFacade.scrape(function (asins) {
-    logger.log("------------");
-    logger.log("FULL SCREEN INCOMING. GRABBING CCC");
-    logger.log("------------");
-    q.push(asins, function (result) {
-      console.log(JSON.stringify(result.determination.asin + " " + result.determination.buy, null, 2));
-
-      if(result.determination.buy) {
-        mailer.mail("CCC", result);
-      }
-    });
-  });
-});
-
-
-
-
+exports.twitterMonitor.monitorKeepa(queue);
+exports.scheduler.startScheduler(queue, 4);
 
 // -------- TESTING STUFFF --------------
 //q.push(["B009C98PR0"], function (result) {
