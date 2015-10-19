d0b43040ec3ef303418ff5a5a6688d41b2eac8d8
Matt Glick <matt.glick@dowjones.com>
2015-10-19 12:50:48 -0400

    made the mailer into a queue such that it won't get overloaded with too many emails at once.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var http = require('http');
 var express = require('express');
 
 var Queue = require('./lib/queue');
+var MailQueue = require('./lib/services/mailer');
 
 var scheduler = require('./lib/services/scheduler');
 var feedFacade = require('./lib/facades/rss-feed-facade');
@@ -15,6 +16,7 @@ var testing = require('./lib/test');
 var server = express();
 
 var queue = new Queue();
+var mailQueue = new MailQueue;
 
 server.set('views', './lib/views');
 server.set('view engine', 'jade');
@@ -39,8 +41,8 @@ server.listen(process.env.PORT || 5000);
 
 console.log("FBA Sourcing Analyzer: Running on Port: " + (process.env.PORT || "5000"));
 
-//twitterMonitor.monitorKeepa(queue);
-//scheduler.startCCCScheduler(queue, 4);
-//scheduler.startTracktorScheduler(queue, 6);
-feedFacade.jungleFeed(queue);
-//testing.test(queue);
+twitterMonitor.monitorKeepa(mailQueue, queue);
+scheduler.startCCCScheduler(mailQueue, queue, 4);
+scheduler.startTracktorScheduler(mailQueue, queue, 6);
+//feedFacade.jungleFeed(mailQueue, queue);
+//testing.test(mailQueue, queue);
